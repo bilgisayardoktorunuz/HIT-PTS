2dd366b7a4ab0efd12d1b87c9ee1ddb2d57d7cc5
bilgisayardoktorunuz <61616505+bilgisayardoktorunuz@users.noreply.github.com>
2020-03-02 17:29:34 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace HIT_PTS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtParola = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.rdbYonetici = new System.Windows.Forms.RadioButton();
+            this.rdbKullanici = new System.Windows.Forms.RadioButton();
+            this.lblUyari = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(123, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 96);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 120);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtKullaniciAdi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtKullaniciAdi.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKullaniciAdi.ForeColor = System.Drawing.Color.White;
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(78, 138);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(200, 19);
+            this.txtKullaniciAdi.TabIndex = 1;
+            this.txtKullaniciAdi.Text = "Kullanıcı Adı";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.ForeColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(16, 184);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 1);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, 239);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // txtParola
+            // 
+            this.txtParola.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.txtParola.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtParola.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtParola.ForeColor = System.Drawing.Color.White;
+            this.txtParola.Location = new System.Drawing.Point(78, 256);
+            this.txtParola.Name = "txtParola";
+            this.txtParola.PasswordChar = '*';
+            this.txtParola.Size = new System.Drawing.Size(200, 19);
+            this.txtParola.TabIndex = 2;
+            this.txtParola.Text = "Parola";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.ForeColor = System.Drawing.Color.Transparent;
+            this.panel2.Location = new System.Drawing.Point(16, 297);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(300, 1);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(174)))), ((int)(((byte)(46)))));
+            this.btnGiris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGiris.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.btnGiris.Location = new System.Drawing.Point(108, 376);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(101, 37);
+            this.btnGiris.TabIndex = 5;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = false;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.btnCikis.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCikis.BackgroundImage")));
+            this.btnCikis.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCikis.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCikis.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCikis.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.btnCikis.Location = new System.Drawing.Point(297, 2);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(30, 26);
+            this.btnCikis.TabIndex = 3;
+            this.btnCikis.UseVisualStyleBackColor = false;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            // 
+            // rdbYonetici
+            // 
+            this.rdbYonetici.AutoSize = true;
+            this.rdbYonetici.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbYonetici.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(166)))), ((int)(((byte)(120)))));
+            this.rdbYonetici.Location = new System.Drawing.Point(60, 330);
+            this.rdbYonetici.Name = "rdbYonetici";
+            this.rdbYonetici.Size = new System.Drawing.Size(78, 20);
+            this.rdbYonetici.TabIndex = 3;
+            this.rdbYonetici.TabStop = true;
+            this.rdbYonetici.Text = "Yönetici";
+            this.rdbYonetici.UseVisualStyleBackColor = true;
+            // 
+            // rdbKullanici
+            // 
+            this.rdbKullanici.AutoSize = true;
+            this.rdbKullanici.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbKullanici.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(100)))), ((int)(((byte)(166)))));
+            this.rdbKullanici.Location = new System.Drawing.Point(180, 330);
+            this.rdbKullanici.Name = "rdbKullanici";
+            this.rdbKullanici.Size = new System.Drawing.Size(81, 20);
+            this.rdbKullanici.TabIndex = 4;
+            this.rdbKullanici.TabStop = true;
+            this.rdbKullanici.Text = "Kullanıcı";
+            this.rdbKullanici.UseVisualStyleBackColor = true;
+            // 
+            // lblUyari
+            // 
+            this.lblUyari.AutoSize = true;
+            this.lblUyari.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblUyari.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.lblUyari.Location = new System.Drawing.Point(93, 440);
+            this.lblUyari.Name = "lblUyari";
+            this.lblUyari.Size = new System.Drawing.Size(15, 16);
+            this.lblUyari.TabIndex = 5;
+            this.lblUyari.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.label1.Location = new System.Drawing.Point(109, 440);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "giriş hakkınız bulunmaktadır.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.label2.Location = new System.Drawing.Point(34, 440);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Toplam";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(330, 482);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblUyari);
+            this.Controls.Add(this.rdbKullanici);
+            this.Controls.Add(this.rdbYonetici);
+            this.Controls.Add(this.btnCikis);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtParola);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtParola;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.Button btnCikis;
+        private System.Windows.Forms.RadioButton rdbYonetici;
+        private System.Windows.Forms.RadioButton rdbKullanici;
+        private System.Windows.Forms.Label lblUyari;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Ses dosyalarını çalıştırabilmemize yarayan kütüphanemizin eklenmesi.
+using System.Media;
+//Veritabanı kütüphanesinin eklenmesi
+using System.Data.OleDb;
+
+namespace HIT_PTS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Veritabanı dosya yolu ve provider nesnesinin belirlenmesi.
+        OleDbConnection baglantim = new OleDbConnection("Provider=Microsoft.Ace.OleDb.12.0;Data Source=personel.accdb");
+
+
+        //Formlar arasında veri aktarımında kullanılacak değişkenler.
+        public static string tcno, adi, soyadi, yetki;
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            this.Close(); //butona tıklandığında programı sonlandırır.
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            if (hak != 0)
+            {
+                baglantim.Open();
+                OleDbCommand selectsorgu = new OleDbCommand("Select * from Kullanicilar", baglantim);
+                OleDbDataReader kayitokuma = selectsorgu.ExecuteReader();
+                while(kayitokuma.Read())
+                {
+                    if(rdbYonetici.Checked==true)
+                    {
+                        if(kayitokuma["kullaniciadi"].ToString()==txtKullaniciAdi.Text&& kayitokuma["parola"].ToString()==txtParola.Text&&kayitokuma["yetki"].ToString()=="Yönetici")
+                        {
+                            durum = true;
+                            tcno = kayitokuma.GetValue(0).ToString();
+                            adi = kayitokuma.GetValue(1).ToString();
+                            soyadi = kayitokuma.GetValue(2).ToString();
+                            yetki = kayitokuma.GetValue(3).ToString();
+                            SoundPlayer baglantisesi = new SoundPlayer(@"C:\Users\RYD - Bilgi İşlem\Desktop\dial.wav");
+                            baglantisesi.Play();
+                            this.Hide();
+                            yoneticipaneli frm2 = new yoneticipaneli();
+                            frm2.Show();
+                            break;
+                        }
+                    }
+
+                    if (rdbKullanici.Checked == true)
+                    {
+                        if (kayitokuma["kullaniciadi"].ToString() == txtKullaniciAdi.Text && kayitokuma["parola"].ToString() == txtParola.Text && kayitokuma["yetki"].ToString() == "Kullanıcı")
+                        {
+                            durum = true;
+                            tcno = kayitokuma.GetValue(0).ToString();
+                            adi = kayitokuma.GetValue(1).ToString();
+                            soyadi = kayitokuma.GetValue(2).ToString();
+                            yetki = kayitokuma.GetValue(3).ToString();
+                            SoundPlayer baglantisesi = new SoundPlayer(@"C:\Users\RYD - Bilgi İşlem\Desktop\dial.wav");
+                            baglantisesi.Play();
+                            this.Hide();
+                            kullanicipaneli frm3 = new kullanicipaneli();
+                            frm3.Show();
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+            if (durum == false)
+                hak--;
+            baglantim.Close();
+            lblUyari.Text = Convert.ToString(hak);
+
+            if(hak==0)
+            {
+                btnGiris.Enabled = false;
+                MessageBox.Show("Giriş Hakkı Kalmadı!", "HİT Personel Takip Sistemi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+
+        //Yerel yani yalnızca bu formda geçerli olacak değişkenler.
+        int hak = 3;bool durum = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Kullanıcı Girişi";
+            this.AcceptButton = btnGiris;this.CancelButton = btnCikis;
+            lblUyari.Text =  Convert.ToString(hak);
+            rdbYonetici.Checked = true;
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
